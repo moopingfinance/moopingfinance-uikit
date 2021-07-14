da613205bd3e0893f4d65a19c5fcaa07f1ea781a
moopingfinance <moopingfinance@gmail.com>
2021-07-14 18:25:44 +0700

    added token icon + spinner icon

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
 import { SpinnerProps } from "./types";
+import { Svg, SvgProps } from "../Svg";
 
 const rotate = keyframes`
   from {
@@ -29,7 +29,16 @@ const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
+const SpinnerIcon: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 96 96" {...props}>
+      <image width="96" height="96" href="/images/spinner.png"/>
+    </Svg>
+  );
+};
+
+
+const RotatingSpinnerIcon = styled(SpinnerIcon)`
   position: absolute;
   top: 0;
   left: 0;
@@ -37,16 +46,11 @@ const RotatingPancakeIcon = styled(PancakeIcon)`
   transform: translate3d(0, 0, 0);
 `;
 
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
-  transform: translate3d(0, 0, 0);
-`;
 
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <RotatingSpinnerIcon width={`${size * 0.5}px`} />
     </Container>
   );
 };
--- src/components/Svg/Icons/PancakeRound.tsx
+++ src/components/Svg/Icons/PancakeRound.tsx
@@ -5,7 +5,8 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 90 90" {...props}>
-      <circle cx="45" cy="45" r="45" fill="url(#paint0_linear)" />
+         <image width={90} height={90} href="/images/tokens/mooping-token.png"/>
+      {/* <circle cx="45" cy="45" r="45" fill="url(#paint0_linear)" />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
@@ -110,7 +111,7 @@ const Icon: React.FC<SvgProps> = (props) => {
           <stop offset="0.370494" stopColor="#7D3900" />
           <stop offset="1" stopColor="#8D4104" />
         </linearGradient>
-      </defs>
+      </defs> */}
     </Svg>
   );
 };
